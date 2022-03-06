de61a3be72c9c19331939710c5dad06c215d1345
Paul Michaels <pcmichaels@live.com>
2022-03-06 11:14:56 +0000

    Delete Program.cs

--- test/datagen.ConsoleApp/Program.cs
+++
@@ -1,7 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-using datagen.MySql;
-
-Console.WriteLine("Hello, World!");
-
-var generate = new Generate("Server=127.0.0.1;Port=3307;Database=musiclenderus;Uid=root;Pwd=mypass;AllowUserVariables=True");
-generate.AddRow();
